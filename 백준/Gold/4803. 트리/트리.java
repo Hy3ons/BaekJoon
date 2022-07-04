c5b5c1425673bbfabd8b6d6b1f22edce5b32bb49
Hy3ons <103893259+Hy3ons@users.noreply.github.com>
2022-07-05 00:45:50 +0900

    [Gold IV] Title: 트리, Time: 472 ms, Memory: 53004 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.java
+++ 백준/Gold/4803. 트리/트리.java
@@ -1,122 +1,70 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
-
-class Pair {
-    int prev, go;
-    Pair (int go, int prev) {
-        this.go = go;
-        this.prev = prev;
-    }
-}
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String input = "";
+        String input;
         int caseCount = 0;
 
-        while(true) {
+        while (true) {
             input = br.readLine();
             if (input.equals("0 0")) break;
-
             caseCount++;
-            StringTokenizer st = new StringTokenizer(input);
 
+            StringTokenizer st = new StringTokenizer(input);
             int testcase = Integer.parseInt(st.nextToken());
             int amount = Integer.parseInt(st.nextToken());
 
-            ArrayList<ArrayList<Integer>> load = new ArrayList<>(testcase+1);
-            for (int i=0;i<testcase+1;i++) load.add(new ArrayList<>());
-
-            int[] parent = new int[testcase+1];
+            parent = new int[testcase+1];
             for (int i=1;i<=testcase;i++) parent[i] = i;
 
-            for (int i=0;i<amount;i++) {
+            while(amount-->0) {
                 st = new StringTokenizer(br.readLine());
                 int n1 = Integer.parseInt(st.nextToken());
                 int n2 = Integer.parseInt(st.nextToken());
 
-                union(parent, n1, n2);
+                if (getParent(n1) == getParent(n2))
+                    union(n1, 0);
 
-                if (n1==n2) {
-                    parent[getParent(parent, n1)] = 0;
-                }
-
-                load.get(n1).add(n2);
-                load.get(n2).add(n1);
+                union(n1, n2);
             }
-
             boolean[] visited = new boolean[testcase+1];
-            Queue<Pair> qu = new LinkedList<>();
-
-            for (int i=1;i<=testcase;i++) {
-                if (!visited[i] && getParent(parent, i)!=0) {
-                    visited[i] = true;
-
-                    qu.offer(new Pair(i, 0));
-                    K: while(!qu.isEmpty()) {
-                        Pair now = qu.poll();
-
-                        for (int go : load.get(now.go)) {
-                            if (now.prev ==go || now.go==go) continue;
-
-                            if (visited[go]) {
-                                union(parent, 0, go);
-
-                                qu.clear();
-                                break K;
-                            } else {
-                                visited[go] = true;
-                                qu.offer(new Pair(go, now.go));
-                            }
-
-                        }
-                    }
-                }
-            }
-
-            Arrays.fill(visited, false);
             int count = 0;
-
-            for (int i=1;i<=testcase;i++) getParent(parent, i);
             for (int i=1;i<=testcase;i++) {
-                if (parent[i]==0) continue;
-                if (!visited[parent[i]]) {
-                    visited[parent[i]] = true;
+                int parent = getParent(i);
+
+                if (parent==0) continue;
+                if (!visited[parent]) {
+                    visited[parent] = true;
                     count++;
                 }
             }
-
             bw.write("Case "+caseCount+": ");
-            if (count==1) {
+            if (count==1)
                 bw.write("There is one tree.\n");
-            } else if (count==0) {
+            else if (count==0)
                 bw.write("No trees.\n");
-            } else {
+            else
                 bw.write("A forest of "+count+" trees.\n");
-            }
         }
         bw.flush();
-
     }
+    public static int[] parent;
 
-    public static void union(int[] parent, int node1, int node2) {
-        int n1 = getParent(parent, node1);
-        int n2 = getParent(parent, node2);
+    public static void union (int node1, int node2) {
+        int n1 = getParent(node1);
+        int n2 = getParent(node2);
 
-        if (n1>n2) {
+        if (n1 > n2)
             parent[n1] = n2;
-        } else {
+        else
             parent[n2] = n1;
-        }
     }
 
-    public static int getParent(int[] parent, int node) {
+    public static int getParent (int node) {
         if (parent[node]==node) return node;
-        return parent[node] = getParent(parent, parent[node]);
+        return parent[node] = getParent(parent[node]);
     }
 }
