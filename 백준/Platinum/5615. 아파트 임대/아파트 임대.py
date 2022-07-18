1aa5f13fa0cf41599d0034c8a8f8f3e1ad99f7d7
Hy3ons <103893259+Hy3ons@users.noreply.github.com>
2022-07-18 12:14:45 +0900

    [Platinum I] Title: 아파트 임대, Time: 4196 ms, Memory: 118820 KB -BaekjoonHub

---
+++ 백준/Platinum/5615. 아파트 임대/아파트 임대.py
@@ -0,0 +1,82 @@
+import math
+import random
+import sys
+
+check = [2,3,5,7,11,13,17,19,23,29,31,37,41,61]
+
+def powing (a, times, MOD) :
+    if times == 0 :
+        return 0
+
+    times -= 1
+    result = a
+    while times > 0 :
+        if times % 2 == 1 :
+            result *= a
+            result %= MOD
+
+        a *= a
+        a %= MOD
+
+        times //= 2
+    return result
+
+def mill (P, a, k, d) :
+    temp = powing(a, d, P)
+    if (temp - 1) % P == 0 :
+        return True
+
+    for i in range(k) :
+        if (temp + 1) % P == 0:
+            return True
+
+        temp *= temp
+        temp %= P
+
+
+def isPrime (P) :
+    if P % 2 == 0 :
+        return False
+
+    for a in check :
+        if P == a :
+            return True
+
+    k = 0
+    d = P-1
+    while d % 2 == 0 :
+        k += 1
+        d /= 2
+
+    for a in check :
+        if not(mill(P, a, k, d)) :
+            return False
+    return True
+
+def nicePrime (P) :
+    if P == 2 :
+        return True
+
+    if P % 2 == 0 :
+        return False
+
+    for i in range(3, int(math.sqrt(P)+1), 2) :
+        if P % i == 0 :
+            return False
+    return True
+
+
+result = 0
+
+length = int(sys.stdin.readline().rstrip())
+list = [int(sys.stdin.readline().rstrip()) for i in range(length)]
+
+for P in list :
+    if P < 4 :
+        result+= 1
+        continue
+
+    if isPrime(P*2+1) :
+        result += 1
+
+print(result)
