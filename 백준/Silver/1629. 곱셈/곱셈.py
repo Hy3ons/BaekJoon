5865987c89971aff360fddb6c03db233607b524e
Hy3ons <103893259+Hy3ons@users.noreply.github.com>
2022-08-25 14:39:06 +0900

    [Silver I] Title: 곱셈, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,14 +1,2 @@
-temp, b, c = map(int, input().split())
-
-a = 1
-while b > 0:
-    if (b & 1) == 1:
-        a *= temp
-        a %= c
-
-    temp *= temp
-    temp %= c
-
-    b >>= 1
-
-print(a % c)
+a, b, c = map(int, input().split())
+print(pow(a, b, c))
