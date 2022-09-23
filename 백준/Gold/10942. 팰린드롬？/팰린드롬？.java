a67078b7881e7426b94abfca7763351b45d7faad
Hy3ons <103893259+Hy3ons@users.noreply.github.com>
2022-09-23 14:16:37 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1064 ms, Memory: 258904 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+
+        for (int i=0;i<arr.length;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean[][] preset = new boolean[N+1][N+1];
+
+        for (int i=0;i<arr.length;i++) {
+            boolean n = true;
+            for (int l = i, r = i;l>=0 && r <arr.length;l--,r++) {
+                if (n && arr[l] == arr[r]) {
+                    preset[l][r] = n;
+                } else {
+                    n = false;
+                }
+            }
+        }
+
+        for (int i=0;i<arr.length-1;i++) {
+            boolean n = arr[i] == arr[i+1];
+            for (int l = i, r = i+1;l>=0 && r<arr.length;l--,r++) {
+                if (n && arr[l] == arr[r]) {
+                    preset[l][r] = n;
+                } else {
+                    n = false;
+                }
+            }
+        }
+        int quer = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while(quer-->0) {
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken()), n2 = Integer.parseInt(st.nextToken());
+            bw.write((preset[n1-1][n2-1] ? 1 : 0)+"\n");
+        }
+        bw.flush();
+
+    }
+}
