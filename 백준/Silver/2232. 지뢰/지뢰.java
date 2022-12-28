1f5a928bf3ddcf14a43b1e104419ae927de69256
Hy3ons <103893259+Hy3ons@users.noreply.github.com>
2022-12-29 05:23:22 +0900

    [Silver II] Title: 지뢰, Time: 688 ms, Memory: 28056 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.java
+++ 백준/Silver/2232. 지뢰/지뢰.java
@@ -2,63 +2,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.concurrent.BrokenBarrierException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int N = Integer.parseInt(br.readLine());
-
         int[] arr = new int[N];
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>() {
-            @Override
-            public int compare(Pair o1, Pair o2) {
-                return Integer.compare(o2.cost, o1.cost);
-            }
-        });
+        PriorityQueue<Pair> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.cost, o1.cost));
 
-        for (int i=0;i<arr.length;i++) {
-            arr[i] = Integer.parseInt(br.readLine() );
-            pq.offer(new Pair(i, arr[i]));
-        }
+        for (int i=0;i<arr.length;i++) 
+            pq.offer(new Pair(i, arr[i] = Integer.parseInt(br.readLine())));
 
 
         boolean[] boom = new boolean[N];
-
         ArrayList<Integer> ans = new ArrayList<>();
-
 
         while(!pq.isEmpty()) {
             Pair now = pq.poll();
             if (boom[now.idx]) continue;
-            
-            boom[now.idx] = true;
 
+            boom[now.idx] = true;
             ans.add(now.idx+1);
-            
-            int limit = now.cost;
-            
+
             for (int i=1 + now.idx;i < arr.length;i++) {
-                if (arr[i] >= limit) break;
-                limit = arr[i];
+                if (arr[i] >= arr[i-1]) break;
                 boom[i] = true;
             }
 
-            limit = now.cost;
             for (int i= now.idx-1;i>=0;i--) {
-                if (arr[i] >= limit) break;
-                limit = arr[i];
+                if (arr[i] >= arr[i+1]) break;
                 boom[i] = true;
             }
         }
 
         Collections.sort(ans);
-        
         for (int e : ans) System.out.println(e);
-
-
-
     }
 }
 
