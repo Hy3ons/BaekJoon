7aad818a203ca11db3fa9625ff5506fb39cf7279
Hy3ons <103893259+Hy3ons@users.noreply.github.com>
2023-01-09 21:46:23 +0900

    [Gold IV] Title: 조교의 맹연습, Time: 28 ms, Memory: 5968 KB -BaekjoonHub

---
+++ 백준/Gold/27114. 조교의 맹연습/조교의 맹연습.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define MAX 1010101
+
+using namespace std;
+
+int A, B, C, K, da[6] = {1,2,0,0,4,0}, db[6] = {1,0,2,0,0,4}, dc[6] = {0,1,1,2,0,0}, dp[MAX];
+
+int main () {
+    cin >> A >> B >> C >> K;
+    fill(dp, dp+MAX, 1e9);
+
+    dp[0] = 0;
+
+    for (int i=0;i<MAX;i++) {
+        for (int k=0;k<6;k++) {
+            int d = da[k] + db[k] + dc[k];
+            int M = da[k] * A + db[k] * B + dc[k] * C;
+
+            if (i + M >= MAX) continue;
+            dp[i + M] = min(dp[i + M], dp[i] + d);
+        }
+    }
+
+    int a = dp[K] == 1e9 ? -1 : dp[K];
+    cout << a;
+}
