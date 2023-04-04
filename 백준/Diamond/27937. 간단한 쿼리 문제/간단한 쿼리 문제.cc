4fc3f1cfded3444514c6046c37e06bc1cfb28579
Hy3ons <103893259+Hy3ons@users.noreply.github.com>
2023-04-04 18:16:38 +0900

    [Diamond IV] Title: 간단한 쿼리 문제, Time: 56 ms, Memory: 17160 KB -BaekjoonHub

--- 백준/Diamond/27937. 간단한 쿼리 문제/간단한 쿼리 문제.cc
+++ 백준/Diamond/27937. 간단한 쿼리 문제/간단한 쿼리 문제.cc
@@ -4,25 +4,18 @@
 
 using namespace std;
 
+int sts(1);
+
 struct seg
 {
     long long tree[MAX_T];
 
-    void update (int node, int s, int e, int left, int right, int value) {
-        if (right < s || e < left) return;
-
-        if (s<= left && right <= e) {
-            tree[node] += value;
-            return;
+    void update (int idx, int value) {
+        for (int i=idx-1+sts;i!=0;i>>=1) {
+            tree[i] += value;
         }
-
-        int mid = left + right >> 1;
-
-        update(node << 1, s, e, left, mid, value);
-        update(node << 1 | 1, s, e, mid+1, right, value);
-
-        tree[node] = tree[node << 1] + tree[node << 1 | 1];
     }
+
 
     long long query (int node, int s, int e, int left, int right) {
         if (right < s || e < left) return 0;
@@ -48,7 +41,7 @@ struct query
     int left, right, idx, sq;
 
     query(int left, int right, int idx) : left(left), right(right), idx(idx) {
-        sq = left / 400;
+        sq = left / 450;
     }
 };
 
@@ -64,6 +57,8 @@ int main () {
     cin.tie(0);ios_base::sync_with_stdio(0);
 
     cin >> N >> Q;
+
+    while(sts < N) sts <<= 1;
 
     for (int i=0;i<N;i++) {
         cin >> arr[i];
@@ -84,44 +79,44 @@ int main () {
             cnt.clear(), value.clear();
 
             for (int i=q.left-1;i<q.right;i++) {
-                temp += cnt.query(1, 1, arr[i], 1, N) * arr[i];
-                temp -= value.query(1, 1, arr[i], 1, N);
-                temp -= cnt.query(1, arr[i], N, 1, N) * arr[i];
-                temp += value.query(1, arr[i], N, 1, N);
+                temp += cnt.query(1, 1, arr[i], 1, sts) * arr[i];
+                temp -= value.query(1, 1, arr[i], 1, sts);
+                temp -= cnt.query(1, arr[i], N, 1, sts) * arr[i];
+                temp += value.query(1, arr[i], N, 1, sts);
 
-                cnt.update(1, arr[i], arr[i], 1, N, 1);
-                value.update(1, arr[i], arr[i], 1, N, arr[i]);
+                cnt.update(arr[i], 1);
+                value.update(arr[i], arr[i]);
             }
         } else {
             for (int i = r; i < q.right;i++) {
-                temp += cnt.query(1, 1, arr[i], 1, N) * arr[i];
-                temp -= value.query(1, 1, arr[i], 1, N);
-                temp -= cnt.query(1, arr[i], N, 1, N) * arr[i];
-                temp += value.query(1, arr[i], N, 1, N);
+                temp += cnt.query(1, 1, arr[i], 1, sts) * arr[i];
+                temp -= value.query(1, 1, arr[i], 1, sts);
+                temp -= cnt.query(1, arr[i], N, 1, sts) * arr[i];
+                temp += value.query(1, arr[i], N, 1, sts);
 
-                cnt.update(1, arr[i], arr[i], 1, N, 1);
-                value.update(1, arr[i], arr[i], 1, N, arr[i]);
+                cnt.update(arr[i], 1);
+                value.update(arr[i], arr[i]);
             }
 
             if (l < q.left) {
                 for (int i=l-1;i<q.left-1;i++) {
-                    cnt.update(1, arr[i], arr[i], 1, N, -1);
-                    value.update(1, arr[i], arr[i], 1, N, -arr[i]);
+                    cnt.update(arr[i], -1);
+                    value.update(arr[i], -arr[i]);
 
-                    temp -= cnt.query(1, 1, arr[i], 1, N) * arr[i];
-                    temp += value.query(1, 1, arr[i], 1, N);
-                    temp += cnt.query(1, arr[i], N, 1, N) * arr[i];
-                    temp -= value.query(1, arr[i], N, 1, N);
+                    temp -= cnt.query(1, 1, arr[i], 1, sts) * arr[i];
+                    temp += value.query(1, 1, arr[i], 1, sts);
+                    temp += cnt.query(1, arr[i], N, 1, sts) * arr[i];
+                    temp -= value.query(1, arr[i], N, 1, sts);
                 }
             } else {
                 for (int i=l-2;i>=q.left-1;i--) {
-                    temp += cnt.query(1, 1, arr[i], 1, N) * arr[i];
-                    temp -= value.query(1, 1, arr[i], 1, N);
-                    temp -= cnt.query(1, arr[i], N, 1, N) * arr[i];
-                    temp += value.query(1, arr[i], N, 1, N);
+                    temp += cnt.query(1, 1, arr[i], 1, sts) * arr[i];
+                    temp -= value.query(1, 1, arr[i], 1, sts);
+                    temp -= cnt.query(1, arr[i], N, 1, sts) * arr[i];
+                    temp += value.query(1, arr[i], N, 1, sts);
 
-                    cnt.update(1, arr[i], arr[i], 1, N, 1);
-                    value.update(1, arr[i], arr[i], 1, N, arr[i]);
+                    cnt.update(arr[i], 1);
+                    value.update(arr[i], arr[i]);
                 }
             }
 
